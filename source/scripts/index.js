0386f320f45e8405d509ff4051c53ee6638c2ce8
KleoSky <161670250+KleoSky@users.noreply.github.com>
2024-09-14 00:00:06 +0700

    Добавляет декоративные элементы
    * Добавляет часть декоров
    
    * Добавляет часть декоров
    
    * Продолжает добавление декоров
    
    * Завершает добавление декоров
    
    * Меняет кота в promo
    
    * Решает конфликты
    
    * Исправляет по PerfectPixel
    
    * Исправляет по PerfectPixel
    
    * Корректирует по Perfect Pixel
    
    * Исправляет Promo
    
    * Добавляет марджин в хэдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-navigation');
+const burger = document.querySelector('.button-burger');
+
+burger.addEventListener('click', ()=> {
+  if (mainNav.classList.contains('main-navigation--closed')) {
+    burger.classList.remove('button-burger--closed');
+    burger.classList.add('button-burger--opened');
+    mainNav.classList.remove('main-navigation--closed');
+    mainNav.classList.add('main-navigation--opened');
+  } else {
+    burger.classList.add('button-burger--closed');
+    burger.classList.remove('button-burger--opened');
+    mainNav.classList.add('main-navigation--closed');
+    mainNav.classList.remove('main-navigation--opened');
+  }
+});
