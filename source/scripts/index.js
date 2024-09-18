3692c032ccc9fa08c69645383533afea50b372ec
KleoSky <161670250+KleoSky@users.noreply.github.com>
2024-09-18 21:15:51 +0700

    Производительность
    * Улучшает производительность
    
    * Работает с переполнением
    
    * Рабатает над переполнением
    
    * Рабатает над переполнением
    
    * Работает над переполнением
    
    * Работает с переполнением
    
    * Автоматизация

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,21 @@
 const mainNav = document.querySelector('.main-navigation');
 const burger = document.querySelector('.button-burger');
+const body = document.querySelector('.body');
+
+body.classList.remove('nojs');
+mainNav.classList.remove('main-navigation--opened');
+mainNav.classList.add('main-navigation--closed');
 
 burger.addEventListener('click', ()=> {
-  if (mainNav.classList.contains('main-navigation--closed')) {
-    burger.classList.remove('button-burger--closed');
-    burger.classList.add('button-burger--opened');
-    mainNav.classList.remove('main-navigation--closed');
-    mainNav.classList.add('main-navigation--opened');
-  } else {
-    burger.classList.add('button-burger--closed');
+  if (mainNav.classList.contains('main-navigation--opened')) {
     burger.classList.remove('button-burger--opened');
-    mainNav.classList.add('main-navigation--closed');
+    burger.classList.add('button-burger--closed');
     mainNav.classList.remove('main-navigation--opened');
+    mainNav.classList.add('main-navigation--closed');
+  } else {
+    burger.classList.add('button-burger--opened');
+    burger.classList.remove('button-burger--closed');
+    mainNav.classList.add('main-navigation--opened');
+    mainNav.classList.remove('main-navigation--closed');
   }
 });
